9c5b4c4d9365d2af61d717da5b265baf4276a968
hjh <hjh970422@gmail.com>
2023-09-12 16:49:33 +0900

    230912

--- eclipse-workspace/board/src/main/java/com/hjh/config/BoardConfig.java
+++ eclipse-workspace/board/src/main/java/com/hjh/config/BoardConfig.java
@@ -1,6 +1,37 @@
 package com.hjh.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.hjh.controller.BoardController;
+import com.hjh.model.BoardDao;
+import com.hjh.model.NoticeboardDto;
+import com.hjh.service.BoardService;
+
+@Configuration
 public class BoardConfig
 {
-
+	@Bean
+	public NoticeboardDto noticeboardDto()
+	{
+		return new NoticeboardDto();
+	}
+	
+	@Bean
+	public BoardDao boardDao()
+	{
+		return new BoardDao();
+	}
+	
+	@Bean
+	public BoardService boardService()
+	{
+		return new BoardService();
+	}
+	
+	@Bean
+	public BoardController boardController()
+	{
+		return new BoardController();
+	}
 }
