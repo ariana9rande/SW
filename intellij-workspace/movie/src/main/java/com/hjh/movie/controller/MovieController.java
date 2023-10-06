c8be09ca490d7ba965fb8782545387bca01f61eb
hjh <hjh970422@gmail.com>
2023-10-06 17:48:04 +0900

    231006

--- intellij-workspace/movie/src/main/java/com/hjh/movie/controller/MovieController.java
+++ intellij-workspace/movie/src/main/java/com/hjh/movie/controller/MovieController.java
@@ -1,8 +1,11 @@
 package com.hjh.movie.controller;
 
 import com.hjh.movie.model.Movie;
+import com.hjh.movie.model.Review;
 import com.hjh.movie.model.User;
 import com.hjh.movie.service.MovieService;
+import com.hjh.movie.service.ReviewService;
+import jakarta.persistence.GeneratedValue;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -10,15 +13,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/movie")
 public class MovieController
 {
     private MovieService movieService;
+    private ReviewService reviewService;
 
-    public MovieController(MovieService movieService)
+    public MovieController(MovieService movieService, ReviewService reviewService)
     {
         this.movieService = movieService;
+        this.reviewService = reviewService;
     }
 
     @GetMapping({"/", "list"})
@@ -50,8 +57,7 @@ public class MovieController
             redirectAttributes.addFlashAttribute("message", "영화 등록이 완료되었습니다.");
 
             return "redirect:" + request.getHeader("Referer");
-        }
-        catch(Exception e)
+        } catch(Exception e)
         {
             String message = e.getMessage();
             redirectAttributes.addFlashAttribute("message", message);
@@ -95,8 +101,7 @@ public class MovieController
             redirectAttributes.addFlashAttribute("message", "영화 정보 수정이 완료되었습니다.");
 
             return "redirect:/movie/edit/" + id;
-        }
-        catch(Exception e)
+        } catch(Exception e)
         {
             String message = e.getMessage();
             redirectAttributes.addFlashAttribute("message", message);
@@ -119,7 +124,7 @@ public class MovieController
                               @RequestParam("password") String password,
                               HttpSession session)
     {
-        User user = (User)session.getAttribute("loggedInUser");
+        User user = (User) session.getAttribute("loggedInUser");
 
         if(!user.getPassword().equals(password))
         {
@@ -134,5 +139,68 @@ public class MovieController
         redirectAttributes.addFlashAttribute("message", "영화 삭제가 완료되었습니다.");
 
         return "redirect:/movie/list";
+    }
+
+    @GetMapping("/view/{id}")
+    public String viewMovie(@PathVariable Long id, Model model)
+    {
+        Movie movie = movieService.getMovie(id);
+        model.addAttribute("movie", movie);
+
+        List<Review> reviewList = reviewService.getReviewList(id);
+        double sum = 0, avg;
+        for(int i = 0; i < reviewList.size(); i++)
+        {
+            sum += reviewList.get(i).getRating();
+        }
+        avg = sum / reviewList.size();
+
+        model.addAttribute("reviewList", reviewList);
+        model.addAttribute("avgRating", avg);
+
+        return "movie/view";
+    }
+
+    @PostMapping("/view/{id}/add-review")
+    public String addReview(@PathVariable Long id,
+                            @RequestParam("rating") Double rating,
+                            @RequestParam(value = "comment", required = false) String comment,
+                            HttpSession session,
+                            RedirectAttributes redirectAttributes)
+    {
+        User user = (User) session.getAttribute("loggedInUser");
+        Movie movie = movieService.getMovie(id);
+
+        try
+        {
+            reviewService.register(rating, comment, user, movie);
+            redirectAttributes.addFlashAttribute("messsage","리뷰 작성이 완료되었습니다.");
+
+            return "redirect:/movie/view/" + id;
+        }
+        catch(Exception e)
+        {
+            String message = e.getMessage();
+            redirectAttributes.addFlashAttribute("message", message);
+
+            return "redirect:/movie/view/" + id;
+        }
+    }
+
+    @PostMapping("/view/{movieId}/edit-review/{reviewId}")
+    public String editReview(@PathVariable Long movieId, @PathVariable Long reviewId,
+                             @RequestParam("rating") Double rating,
+                             @RequestParam(value = "comment", required = false) String comment,
+                             HttpSession session,
+                             RedirectAttributes redirectAttributes)
+    {
+        Review review = reviewService.getReview(reviewId);
+
+        review.setRating(rating);
+        review.setComment(comment);
+
+        reviewService.update(review);
+
+        return "redirect:/movie/view/" + movieId;
     }
 }
