fb36aa98f61e2a3f6b57699aa89e2cb1b945ede2
hjh <hjh970422@gmail.com>
2023-10-10 10:57:19 +0900

    231010

--- intellij-workspace/user/src/main/java/com/hjh/user/controller/DiscoveryController.java
+++ intellij-workspace/user/src/main/java/com/hjh/user/controller/DiscoveryController.java
@@ -2,12 +2,21 @@ package com.hjh.user.controller;
 
 import com.hjh.user.service.DiscoveryService;
 import org.springframework.beans.factory.annotation.Autowired;
+<<<<<<< HEAD
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+=======
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
 @Controller
+>>>>>>> origin/master
 public class DiscoveryController
 {
     @Autowired
