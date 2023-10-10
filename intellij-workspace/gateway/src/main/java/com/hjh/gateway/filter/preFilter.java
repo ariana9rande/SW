f6e8f246643d7ad7083921626a56deddc03cef3f
hjh <hjh970422@gmail.com>
2023-10-10 12:29:52 +0900

    231010

---
+++ intellij-workspace/gateway/src/main/java/com/hjh/gateway/filter/preFilter.java
@@ -0,0 +1,5 @@
+package com.hjh.gateway.filter;
+
+public class preFilter
+{
+}
---
+++ intellij-workspace/gateway/src/test/java/com/hjh/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjh.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
