9a1307fa299ea0f66f9f73d0e1acb3af5db019f2
hjh <hjh970422@gmail.com>
2023-10-11 10:54:36 +0900

    231011

--- intellij-workspace/board/src/main/java/com/hjh/board/controller/BoardController.java
+++ intellij-workspace/board/src/main/java/com/hjh/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.hjh.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -12,9 +13,11 @@ public class BoardController
     @Autowired
     RestTemplate restTemplate;
 
-    @GetMapping("/test")
-    public String test()
+    @GetMapping("/showAll")
+    public String selectAll(Model model)
     {
-        return "BoardController.test";
+        
+
+        return "board/showAll";
     }
 }
---
+++ intellij-workspace/ui/src/main/webapp/WEB-INF/views/board/showAll.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>게시판</title>
+</head>
+<body>
+<h1>Board</h1>
+</body>
+</html>
