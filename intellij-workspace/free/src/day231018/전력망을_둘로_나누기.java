6262f547eb64fadcaaa0427036a64710bd373568
hjh <hjh970422@gmail.com>
2023-10-19 13:56:52 +0900

    231019

--- intellij-workspace/free/src/day231018/전력망을_둘로_나누기.java
+++ intellij-workspace/free/src/day231018/전력망을_둘로_나누기.java
@@ -4,6 +4,7 @@ public class 전력망을_둘로_나누기
 {
     public int solution(int n, int[][] wires)
     {
+
         int answer = -1;
         return answer;
     }
---
+++ intellij-workspace/free/src/day231019/Free.java
@@ -0,0 +1,11 @@
+package day231019;
+
+import java.util.ArrayList;
+
+public class Free
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
